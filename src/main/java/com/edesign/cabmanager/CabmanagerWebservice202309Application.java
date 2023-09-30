81a7716cb7ca6a9feabf13689d270ab881848b25
ecolombo <elia.colombo@gmail.com>
2023-09-30 17:58:41 +0200

    First commit: Initializr package

---
+++ src/main/java/com/edesign/cabmanager/CabmanagerWebservice202309Application.java
@@ -0,0 +1,13 @@
+package com.edesign.cabmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CabmanagerWebservice202309Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CabmanagerWebservice202309Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/edesign/cabmanager/CabmanagerWebservice202309ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edesign.cabmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CabmanagerWebservice202309ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
